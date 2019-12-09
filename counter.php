99707394710573898cada40d6bb448ba1c79ac32
septianferry13 <42851687+septianferry13@users.noreply.github.com>
2019-12-09 10:51:44 +0700

    Add files via upload

---
+++ counter.php
@@ -0,0 +1,9 @@
+<?php
+//harviacode.com
+$filecounter=("counter.txt");
+$kunjungan=file($filecounter);
+$kunjungan[0]++;
+$file=fopen($filecounter,"w");
+fputs($file,"$kunjungan[0]");
+fclose($file);
+?>
