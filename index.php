3a75110ceb62495a5e735ebe7efd24dcb0b86c73
septianferry13 <42851687+septianferry13@users.noreply.github.com>
2020-01-07 18:25:56 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,192 @@
+<?php
+session_start(); #list: key, msisdn, otp, secret_token
+?>
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Tembak Telkomsel © 2020</title>
+    <link rel="shortcut icon" href="https://resources.1337route.cf/favicon.ico" type="image/x-icon" />
+    <meta charset="UTF-8">
+    <link rel="stylesheet" type="text/css" href="https://colorlib.com/etc/cf/ContactFrom_v10/vendor/bootstrap/css/bootstrap.min.css">
+    <link rel="stylesheet" type="text/css" href="https://colorlib.com/etc/cf/ContactFrom_v10/fonts/font-awesome-4.7.0/css/font-awesome.min.css">
+    <link rel="stylesheet" type="text/css" href="https://colorlib.com/etc/cf/ContactFrom_v10/vendor/animate/animate.css">
+    <link rel="stylesheet" type="text/css" href="https://colorlib.com/etc/cf/ContactFrom_v10/vendor/css-hamburgers/hamburgers.min.css">
+    <link rel="stylesheet" type="text/css" href="https://colorlib.com/etc/cf/ContactFrom_v10/vendor/animsition/css/animsition.min.css">
+    <link rel="stylesheet" type="text/css" href="https://colorlib.com/etc/cf/ContactFrom_v10/vendor/select2/select2.min.css">
+    <link rel="stylesheet" type="text/css" href="https://colorlib.com/etc/cf/ContactFrom_v10/vendor/daterangepicker/daterangepicker.css">
+    <link rel="stylesheet" type="text/css" href="https://colorlib.com/etc/cf/ContactFrom_v10/css/util.css">
+    <link rel="stylesheet" type="text/css" href="https://colorlib.com/etc/cf/ContactFrom_v10/css/main.css">
+</head>
+<?php
+    date_default_timezone_set('Asia/Jakarta');
+    
+    require_once('config.php');
+    require('class.php');
+    
+    $err    = NULL;
+    $ress   = NULL;
+    
+    if (isset($_POST) and isset($_POST['do'])){
+        
+        switch($_POST['do']){
+            
+            default: die(); exit(); break;
+
+            case "CHANGE":{
+                $msisdn = $_SESSION['msisdn'];
+                $tipe   = $_SESSION['tipe'];
+                
+                unset($_SESSION['tipe']);
+                unset($_SESSION['msisdn']);
+                unset($_SESSION['otp']);
+                unset($_SESSION['secret_token']);
+                session_destroy();
+            }
+            break;
+            
+            case "LOGOUT":{
+                
+                $msisdn         = $_SESSION['msisdn'];
+                $tipe           = $_SESSION['tipe'];
+                $otp            = $_SESSION['otp'];
+                $secret_token   = $_SESSION['secret_token'];
+                
+                
+                $tsel = new MyTsel();
+                $tsel->logout($secret_token, $tipe);
+                
+                unset($_SESSION['tipe']);
+                unset($_SESSION['msisdn']);
+                unset($_SESSION['otp']);
+                unset($_SESSION['secret_token']);
+                session_destroy();
+            }
+            break;
+            
+            
+            case "GETOTP":{
+                $msisdn = $_POST['msisdn'];
+                
+                
+
+                $tsel = new MyTsel();
+                if ($tsel->get_otp($msisdn) == "SUKSES"){
+                    
+                    session_regenerate_id();
+                    $_SESSION['msisdn'] = $msisdn;                    
+                    session_write_close();
+
+                }
+                else
+                {
+                    $err = "Error: msisdn salah";
+                }
+            }
+            break;
+            
+            case "LOGIN":{
+                $msisdn = $_SESSION['msisdn'];
+                $tipe   = $_POST['tipe'];
+                $otp    = $_POST['otp'];
+                
+                //if ($key != privatekey){die("Error: wrong key");}
+                $tsel = new MyTsel();
+                $login = $tsel->login($msisdn, $otp, $tipe);
+                
+                
+                if (strlen($login) > 0){
+
+                    $secret_token               = trim(preg_replace('/\s+/', ' ', $login));
+                    $_SESSION['otp']            = $otp;
+                    $_SESSION['secret_token']   = $secret_token;
+                    $_SESSION['tipe']           = $tipe;
+                    
+                    
+                } else {
+                    echo $login;
+                    $err = "PKGID: <b>".$pkgid."</b><br>Result: ".$tsel->buy_pkg($secret_token, $pkgid, $transactionid, $tipe);
+                }
+
+                
+            }
+            break;
+            
+            case "BUY_PKG":{
+                $msisdn         = $_SESSION['msisdn'];
+                $tipe           = $_SESSION['tipe'];
+                $secret_token   = $_SESSION['secret_token'];
+                $pkgid          = $_POST['pkgid'];
+                $transactionid  = $_POST['transactionid'];
+                
+                switch($_POST['pkgid']){
+                case '1':
+                    $pkgidman = $_POST['pkgidman'];
+                    $tsel = new MyTsel();
+                    $ress = "PKGID: <b>".$pkgidman."</b><br>Result: ".$tsel->buy_pkg($secret_token, $pkgidman, $transactionid, $tipe);
+                break;
+                default:
+                    $tsel = new MyTsel();
+                    $ress = "PKGID: <b>".$pkgid."</b><br>Result: ".$tsel->buy_pkg($secret_token, $pkgid, $transactionid, $tipe);
+                }
+                
+            }
+            break;
+            
+        }
+        
+    }
+?>
+
+<!-- ################################ 1 ################################ -->
+<?php if (!isset($_SESSION['msisdn']) and !isset($_SESSION['otp']) and !isset($_SESSION['secret_token']) ){ ?>
+<body>
+<div class="container-contact100">
+<div class="wrap-contact100">
+<form class="contact100-form validate-form" method="POST">
+<span class="contact100-form-title">
+MAU NGAPAIN LU ANJING
+BAKEKOK BUNDA
+</span>
+<!--     <form method="POST">
+    <pre> -->
+<div class="wrap-input100 validate-input" data-validate="Please enter your msisdn">
+<input class="input100" type="text" name="msisdn" placeholder="Nomer Hp 628x">
+<span class="focus-input100"></span>
+</div>
+    <center>
+<button class="contact100-form-btn" name="do" value="GETOTP" type="submit">
+    <span>
+<i class="fa fa-paper-plane-o m-r-6" aria-hidden="true"></i>
+GET OTP
+</span></button>
+</div>
+<!-- <input type="submit" name="do" value="GETOTP"></input> -->
+<?php if(!empty($err)) echo $err ?> 
+<!--     </pre> -->
+   <center>
+    <div class="container">
+            <?php
+$filename = 'counter.txt';
+ 
+function counter(){  
+ global $filename; 
+ 
+ if(file_exists($filename)){  
+  $value = file_get_contents($filename); 
+ }else{  
+  $value = 0;  
+ }
+ 
+ file_put_contents($filename, ++$value);  
+}
+ 
+counter(); 
+ 
+echo 'Total Pengguna : '.file_get_contents($filename); 
+?>
+            USER
+</form>
+</div>
+</div>
+</body>
+<?php } ?>
